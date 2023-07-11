540ea68a736e0473c6dafc01f02d87c480701c39
Antonio Martinez <antoniomrtinez@gmail.com>
2023-07-10 18:38:46 -0700

    two bug fixes

--- src/trader_text_edit.h
+++ src/trader_text_edit.h
@@ -14,7 +14,8 @@ internal void text_edit_insert_string(Text_Edit_Buffer *teb, String_utf8 string)
   expect(teb->encoding == string_encoding_utf8);
 
   u64 one_past_string_last_char_pos = get_last_char_pos(string) + 1;
-  if ((teb->buf.used + one_past_string_last_char_pos) < teb->buf.size)
+  if ((teb->next_char_index <= teb->buf.used) &&
+      ((teb->buf.used + one_past_string_last_char_pos) <= teb->buf.size))
   {
     u8 *next_char_pos = teb->buf.data + teb->next_char_index;
     move_memory_block(next_char_pos + one_past_string_last_char_pos, next_char_pos, one_past_string_last_char_pos);

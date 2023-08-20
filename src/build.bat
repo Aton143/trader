cf999b6e6b255bbf86e0367253e95e93434a8185
Antonio Martinez <antoniomrtinez@gmail.com>
2023-08-20 14:26:55 -0700

    There were changes apparently

--- src/build.bat
+++ src/build.bat
@@ -15,6 +15,6 @@ pushd ..\build
 
 del *.pdb > NUL 2> NUL
 
-cl ..\src\platform_win32\win32_trader.cpp %INCLUDE_DIRS% /nologo /Od /Oi /FC /Z7 /WX /W4 /MT /Gm- /GR- /wd4201 /wd4706 /EHa- /EHc- /EHsc- /wd4505 /link %LIBS% /incremental:no /opt:ref
+cl ..\src\platform_win32\win32_trader.cpp %INCLUDE_DIRS% /nologo /O2 /Oi /FC /Z7 /WX /W4 /MT /Gm- /GR- /wd4201 /wd4706 /EHa- /EHc- /EHsc- /wd4505 /link %LIBS% /incremental:no /opt:ref
 
 popd
